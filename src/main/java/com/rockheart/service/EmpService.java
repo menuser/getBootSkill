b20ac78837ff9cb1ff2b7357759fd5bd6de44bd2
menuser <zhaolei152624@163.com>
2022-11-11 11:11:13 +0800

    执行日志及相关配置类

---
+++ src/main/java/com/rockheart/service/EmpService.java
@@ -0,0 +1,10 @@
+package com.rockheart.service;
+
+public class EmpService{
+
+    public void add() {
+        //todo
+    }
+
+
+}
--- src/test/java/com/rockheart/test1/SpringbootConfigTest02.java
+++ src/test/java/com/rockheart/test1/SpringbootConfigTest02.java
@@ -3,24 +3,23 @@ package com.rockheart.test1;
 import com.rockheart.po.Emp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runner.notification.RunListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.stream.Stream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootConfigTest {
+public class SpringbootConfigTest02 {
 
     @Autowired
-    Emp emp;
+    ApplicationContext context;
 
     @Test
-    public void contextLoadTest(){
-        System.out.println(emp);
+    public void testXml(){
+//        System.out.println("empService"+context.getBean("empService"));
+        System.out.println("empService"+context.getBean("empService2"));
     }
+
 
 }
