6a2ec5ea4b6d54ca794471c3bc21d29b392dbaa1
menuser <zhaolei152624@163.com>
2022-11-15 15:49:59 +0800

    相关页面标签提交

--- src/main/java/com/rockheart/config/ThymeleafConfig.java
+++ src/main/java/com/rockheart/config/ThymeleafConfig.java
@@ -1,30 +1,29 @@
 package com.rockheart.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.nio.charset.Charset;
 
-//@Configurable
-//@Component
+//@ConfigurationProperties(prefix = "spring.thymeleaf")
+//@Configuration
 public class ThymeleafConfig {
     private static final Charset DEFAULT_ENCODING = Charset.defaultCharset();
     public static final String DEFAULT_PREFIX = "classpath:/template/";
     public static final String DEFAULT_SUFFIX = ".html";
 
-    @Bean
-    public ThymeleafProperties getThymeleafProperties(){
-        ThymeleafProperties thymeleafProperties = new ThymeleafProperties();
-        thymeleafProperties.setCache(false);
-        thymeleafProperties.setEncoding(DEFAULT_ENCODING);
-        thymeleafProperties.setPrefix(DEFAULT_PREFIX);
-        thymeleafProperties.setSuffix(DEFAULT_SUFFIX);
-        return thymeleafProperties;
-    }
+//    @Bean
+//    public ThymeleafProperties getThymeleafProperties(){
+//        ThymeleafProperties thymeleafProperties = new ThymeleafProperties();
+//        thymeleafProperties.setCache(false);
+//        thymeleafProperties.setEncoding(DEFAULT_ENCODING);
+//        thymeleafProperties.setPrefix(DEFAULT_PREFIX);
+//        thymeleafProperties.setSuffix(DEFAULT_SUFFIX);
+//        return thymeleafProperties;
+//    }
 
 
 }
